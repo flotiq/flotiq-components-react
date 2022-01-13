2738ca859aa3d4d44214ff57c3b8128ec925cb90
michal.chrzaszcz <mchrzaszcz90@gmail.com>
2022-01-13 12:20:07 +0100

    Add button component example

--- dist/Button/Button.js
+++ dist/Button/Button.js
@@ -11,60 +11,33 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 
 require("../index.css");
 
-require("./button.css");
-
-var _excluded = ["primary", "backgroundColor", "size", "label", "rounded"];
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
 
 /**
  * Primary UI component for user interaction
  */
 var Button = function Button(_ref) {
-  var primary = _ref.primary,
-      backgroundColor = _ref.backgroundColor,
+  var label = _ref.label,
       size = _ref.size,
-      label = _ref.label,
-      _ref$rounded = _ref.rounded,
-      rounded = _ref$rounded === void 0 ? '2xl' : _ref$rounded,
-      props = _objectWithoutProperties(_ref, _excluded);
-
-  var mode = primary ? 'bg-blue-900' : 'bg-red-900';
-  var borderRadius = {
-    '2xl': 'rounded-2xl',
-    xl: 'rounded-xl'
+      onClick = _ref.onClick;
+  var sizeClass = {
+    sm: 'px-8 py-3 text-sm',
+    md: 'px-12 py-4 text-md',
+    lg: 'px-16 py-5 text-lg'
   };
-  return /*#__PURE__*/_react.default.createElement("button", _extends({
+  return /*#__PURE__*/_react.default.createElement("button", {
     type: "button",
-    className: ['mt-20', borderRadius[rounded], 'storybook-button', "storybook-button--".concat(size), mode].join(' '),
-    style: backgroundColor && {
-      backgroundColor: backgroundColor
-    }
-  }, props), label);
+    onClick: onClick,
+    className: "\n                inline-flex\n                items-center\n                ".concat(sizeClass[size], "\n                leading-4\n                font-medium\n                rounded-full\n                shadow-sm\n                text-white\n                bg-primary\n                hover:bg-primary-600\n                transition-colors\n                duration-200\n                ease-in-out\n                focus:outline-none")
+  }, label);
 };
 
 exports.Button = Button;
 Button.propTypes = {
   /**
-  * Is this the principal call to action on the page?
-  */
-  primary: _propTypes.default.bool,
-
-  /**
-  * What background color to use
-  */
-  backgroundColor: _propTypes.default.string,
-
-  /**
-  * How large should the button be?
-  */
-  size: _propTypes.default.oneOf(['small', 'medium', 'large']),
+   * How large should the button be?
+   */
+  size: _propTypes.default.oneOf(['sm', 'md', 'lg']),
 
   /**
   * Button contents
@@ -72,19 +45,11 @@ Button.propTypes = {
   label: _propTypes.default.string.isRequired,
 
   /**
-   * Border radius
-   */
-  rounded: _propTypes.default.oneOf(['xl', '2xl']),
-
-  /**
   * Optional click handler
   */
   onClick: _propTypes.default.func
 };
 Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  rounded: '2xl',
-  onClick: undefined
+  onClick: undefined,
+  size: 'sm'
 };
