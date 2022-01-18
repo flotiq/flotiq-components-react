ae15b8d5a05956fdd462320905db39100e1adace
Kinga <knapik.kinga@gmail.com>
2022-01-18 15:15:08 +0100

    pagination component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Announcement from './Announcement/Announcement';
 import Button from './Button/Button';
+import Pagination from './Pagination/Pagination';
 
 export {
     Announcement,
     Button,
+    Pagination,
 };
