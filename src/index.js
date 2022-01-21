8a276d515d18dc915f6416b234c9c262f70985b0
Kinga <knapik.kinga@gmail.com>
2022-01-21 15:47:04 +0100

    content component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import Announcement from './components/Announcement/Announcement';
+import Audio from './components/Audio/Audio';
 import Button from './components/Button/Button';
+import Code from './components/Code/Code';
+import Content from './components/Content/Content';
+import Delimiter from './components/Delimiter/Delimiter';
+import File from './components/File/File';
+import Header from './components/Header/Header';
+import Image from './components/Image/Image';
+import List from './components/List/List';
 import Pagination from './components/Pagination/Pagination';
+import Paragraph from './components/Paragraph/Paragraph';
+import Quote from './components/Quote/Quote';
+import Table from './components/Table/Table';
+import Video from './components/Video/Video';
+import Warning from './components/Warning/Warning';
+import YouTubeEmbed from './components/YouTubeEmbed/YouTubeEmbed';
 
 export {
     Announcement,
+    Audio,
     Button,
+    Code,
+    Content,
+    Delimiter,
+    File,
+    Header,
+    Image,
+    List,
     Pagination,
+    Paragraph,
+    Quote,
+    Table,
+    Video,
+    Warning,
+    YouTubeEmbed,
 };
