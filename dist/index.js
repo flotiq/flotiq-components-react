5e003420e8e513d35b43e7bbc38304e221012f9a
trzcina <michal.chrzaszcz@wp.pl>
2022-01-25 13:22:13 +0100

    Compile current state to dist

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import Announcement from './components/Announcement/Announcement';
-import Button from './components/Button/Button';
-import Card from './components/Card/Card';
-import Pagination from './components/Pagination/Pagination';
-export { Announcement, Button, Card, Pagination };
