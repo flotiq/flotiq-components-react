4423ccb58f1a96f031ef6875739db57ea68eb9c6
Karol Grochowalski <karol.grochowalski@gmail.com>
2022-02-17 14:34:06 +0100

    add babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = (api) => {
     api.cache(true);
 
-    const presets = ['@babel/preset-react'];
+    const presets = ['@babel/preset-env', '@babel/preset-react'];
     const plugins = ['macros'];
 
     return {
