1b27723b309ae0ebbfbf6134ba57d94ec6fb2311
Michal Chrzaszcz <michal.chrzaszcz@codewave.pl>
2022-10-18 10:53:43 +0200

    Remove prefix, preserve fq-base

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,8 @@ module.exports = (ctx) => ({
     map: ctx.options.map,
     parser: ctx.options.parser,
     plugins: {
-        'postcss-import': { root: ctx.file.dirname },
+        'postcss-import': {},
         tailwindcss: {},
         autoprefixer: {},
     },
-})
+});
