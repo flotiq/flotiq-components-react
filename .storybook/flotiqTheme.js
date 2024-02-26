9af4fd2731083849bf7d2856dc32fd235f794fc7
Maciej Labedzki <maciej.labedzki@codewave.pl>
2024-02-27 00:10:51 +0100

    Release 1.1.4

--- .storybook/flotiqTheme.js
+++ .storybook/flotiqTheme.js
@@ -3,8 +3,6 @@ import { create } from '@storybook/theming';
 export default create({
     base: 'light',
     brandTitle: 'Flotiq components for React',
-    brandUrl: 'https://flotiq.com',
-    brandImage: 'https://editor.flotiq.com/fonts/fq-logo.svg',
-
+    brandUrl: 'https://flotiq.com',    
     fontBase: '"Inter", sans-serif',
 });
